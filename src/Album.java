2acb414a6da1585c68341b6a5fc3909f5598d332
LukaA <luka.alhonen@protonmail.com>
2021-04-06 11:57:18 +0300

    Added documentation

--- src/Album.java
+++ src/Album.java
@@ -8,18 +8,18 @@ import java.util.ArrayList;
 import java.util.*;
 
 public class Album {
-    private final int MAX_SUBALBUMS = 500;
-    private int numSubAlbums;
-    private Album parent;
+    private final int MAX_SUBALBUMS = 500; // Max antal subalbum
+    private int numSubAlbums; // Håller reda på mängden subalbum
     private String name;
-    private List<Album> SubAlbum = new ArrayList<Album>();
-    private List<SoundClip> LjudFiler = new ArrayList<SoundClip>();
+    private List<Album> SubAlbum = new ArrayList<Album>(); // Arraylist för att representera SubAlbum
+    private List<SoundClip> LjudFiler = new ArrayList<SoundClip>(); // Arraylist för att representera LjudFiler
 
     // Simpel construktor
     public Album(String name){
         this.name = name;
     }
 
+    // Simpel construktor med default namn
     public Album(){
         this.name = "New Album";
     }
@@ -29,20 +29,29 @@ public class Album {
         return name;
     }
 
+    // Returnerar ljudfilen vid indexet
     public SoundClip getSoundClip(int index){
+        if(LjudFiler.size() == 0){
+            return null;
+        }
         return LjudFiler.get(index);
     }
 
+    // Returnerar den sista ljudfilen
     public SoundClip getSoundClip(){
         if(LjudFiler.size() == 0){
             return null;
         }
-
         return LjudFiler.get(LjudFiler.size() - 1);
     }
 
     // Returnerar subalbumet vid indexet
-    public Album getSubAlbum(int index){ return SubAlbum.get(index); }
+    public Album getSubAlbum(int index){
+        if(SubAlbum.size() == 0){
+            return null;
+        }
+        return SubAlbum.get(index);
+    }
 
     // Returnerar det sista subalbumet
     public Album getSubAlbum(){
@@ -53,10 +62,12 @@ public class Album {
     }
 
     // Setters
+    // Lägger till en ljudfil, ingen gräns på mängden ljudfiler i album
     public void addSoundClip(SoundClip song){
         LjudFiler.add(song);
     }
 
+    // Lägger till ett subalbum om det finns rum
     public void addSubAlbum(Album subalbum){
         if(numSubAlbums < MAX_SUBALBUMS) {
             SubAlbum.add(subalbum);
@@ -64,26 +75,35 @@ public class Album {
         }
     }
 
+    // Tar bort subalbumet vid indexet och returnerar det
     public Album removeSubAlbum(int index){
+        if(SubAlbum.size() == 0){
+            return null;
+        }
         return SubAlbum.remove(index);
     }
 
+    // Tar bort det sista subalbumet och returnerar det
     public Album removeSubAlbum(){
         if(SubAlbum.size() == 0){
             return null;
         }
         return SubAlbum.remove(SubAlbum.size() - 1);
     }
-    
+
+    // Tar bort ljudfilen vid indexet och returnerar den
     public SoundClip removeSoundClip(int index){
+        if(LjudFiler.size() == 0){
+            return null;
+        }
         return LjudFiler.remove(index);
     }
 
+    // Tar bort den sista ljudfilen och returnerar den
     public SoundClip removeSoundClip(){
         if(LjudFiler.size() == 0){
             return null;
         }
-
         return LjudFiler.remove(LjudFiler.size() - 1);
     }
 
